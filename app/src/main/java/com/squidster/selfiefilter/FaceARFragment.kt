a1db5d24d103a6ed0eb0e2dd32bf4c4b8dba6125
Taylor Ferens <tkferens@hotmail.com>
2020-08-20 17:06:11 -0500

    first commit

---
+++ app/src/main/java/com/squidster/selfiefilter/FaceARFragment.kt
@@ -0,0 +1,28 @@
+package com.squidster.selfiefilter
+
+import android.os.Bundle
+import android.view.View
+import com.google.ar.core.Config
+import com.google.ar.core.Session
+import com.google.ar.sceneform.ux.ArFragment
+import java.util.*
+
+class FaceARFragment : ArFragment(){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Disable plane discovery as we are detecting faces in this app
+        planeDiscoveryController.hide()
+        planeDiscoveryController.setInstructionView(null)
+    }
+
+    override fun getSessionFeatures(): MutableSet<Session.Feature> {
+        return EnumSet.of(Session.Feature.FRONT_CAMERA)
+    }
+
+    override fun getSessionConfiguration(session: Session?): Config {
+        val config = Config(session)
+        config.augmentedFaceMode = Config.AugmentedFaceMode.MESH3D
+        return config
+    }
+}
